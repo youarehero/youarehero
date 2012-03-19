90cf9661f732a41074820432bbb8a482db34ba0d
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-03-19 15:52:06 +0100

    move project_root up

--- src/projecttemplate/settings/base.py
+++ src/projecttemplate/settings/base.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 settings_dir = os.path.dirname(__file__)
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(settings_dir), '../'))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(settings_dir), '../../'))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -54,7 +54,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.realpath(os.path.join(PROJECT_ROOT, "../media"))
+MEDIA_ROOT = os.path.realpath(os.path.join(PROJECT_ROOT, "media"))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -66,7 +66,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.realpath(os.path.join(PROJECT_ROOT, "../static"))
+STATIC_ROOT = os.path.realpath(os.path.join(PROJECT_ROOT, "static"))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
