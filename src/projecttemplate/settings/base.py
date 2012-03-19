d53de9cc4087be72305a0118dd46e188eacfad8f
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-03-19 15:50:46 +0100

    correct folders

--- src/projecttemplate/settings/base.py
+++ src/projecttemplate/settings/base.py
@@ -54,7 +54,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.realpath(os.path.join(PROJECT_ROOT, "media"))
+MEDIA_ROOT = os.path.realpath(os.path.join(PROJECT_ROOT, "../media"))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -66,7 +66,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.realpath(os.path.join(PROJECT_ROOT, "static"))
+STATIC_ROOT = os.path.realpath(os.path.join(PROJECT_ROOT, "../static"))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
