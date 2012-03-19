304892af3d04ba09261718949a79412de1edb4cf
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-03-19 09:45:17 +0100

    yeah

--- projecttemplate/urls.py
+++ projecttemplate/urls.py
@@ -15,3 +15,16 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from django.conf import settings
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns = patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+            }),
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+            }),
+    ) + urlpatterns
+
