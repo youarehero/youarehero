27adbe8a904871719cdac63354c6721abbc64237
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-05-12 11:44:49 +0200

    fix some stuff

--- src/herobase/views.py
+++ src/herobase/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from django.utils.decorators import method_decorator
 
@@ -49,9 +50,11 @@ class QuestDetailView(DetailView):
 def home_view(request):
     if request.user.is_authenticated():
         return hero_home_view(request)
-    return render(request, "herobase/public_home.html", {'open_quests': Quest.objects.filter(state=Quest.STATE_OPEN)})
+    return render(request, "herobase/public_home.html", {'open_quests':
+        Quest.objects.filter(state=Quest.STATE_OPEN)})
 
 
+@login_required
 def hero_home_view(request):
     hero = request.user
     return render(request, 'herobase/hero_home.html',
@@ -60,19 +63,17 @@ def hero_home_view(request):
              'adventures': hero.adventures.order_by('-created'),
              'authored_quests': hero.authored_quests.order_by('-created')})
 
-
-@require_http_methods(["POST"])
+@require_POST
 @login_required
 def adventure_update(request, quest_id):
     quest = get_object_or_404(Quest, pk=quest_id)
-    if quest.author == request.user:
-        messages.error(request, 'You are the author.')
-        return render(request, 'herobase/quest/detail_for_author.html', {'quest': quest})
-    elif request.user in User.objects.filter(quests=quest):
-        messages.info(request, 'You are already applying for the quest "%s".' % quest.title)
-        return render(request, 'herobase/quest/detail_for_hero.html', {'quest': quest})
-    else: #hero not assigned yet
-        adventure = Adventure.objects.create(user=request.user, quest=quest, state=Adventure.STATE_APPLIED)
-        adventure.save()
-        messages.success(request, 'You are a hero!')
-    return render(request, 'herobase/quest/adventure_update.html', {'quest': quest})
+    if 'apply' in request.POST:
+        if request.user == quest.author:
+            messages.error("You can't participate in your own quest")
+        elif request.user in quest.heroes.all():
+            messages.info(request, 'You are already applying for the quest "%s".' % quest.title)
+            return render(request, 'herobase/quest/detail_for_hero.html', {'quest': quest})
+        else:
+            adventure = Adventure.objects.create(user=request.user, quest=quest, state=Adventure.STATE_APPLIED)
+            messages.success(request, 'You are a hero!')
+    return HttpResponseRedirect(reverse("quest-detail", args=(quest.pk,)))
