32fc218278ad92431ae68f29b7e5d74af798f2fc
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-05-14 18:26:49 +0200

    fix base test

--- src/herobase/tests.py
+++ src/herobase/tests.py
@@ -18,29 +18,21 @@ class BehaviourBase(object):
     def step_i_access_the_url(self, url):
         r'I access the url "(.*)"'
         self.url = url
+        self.client = Client()
 
     def step_i_am_the_user(self, username):
         'I am the user "(.*)"'
         self.user = User.objects.create(username=username)
-        self.user_password = 'pw'
-        self.user.set_password(self.user_password)
+        self.user.set_password('pw')
         self.user.save()
-
-    @property
-    def response(self):
-        if not hasattr(self, '_response'):
-            self.client = Client()
-            if hasattr(self, 'user'):
-                login = self.client.login(username=self.user.username,
-                    password=self.user_password)
-                self.assertTrue(login)
-            self.assertTrue(hasattr(self, 'url'))
-            self._response = self.client.get(self.url)
-        return self._response
+        login = self.client.login(username=self.user.username,
+            password='pw')
+        assert login
 
     def step_i_see_the_text(self, text):
         r'I see the text "(.+)"'
-        self.assertContains(self.response, text)
+        response = self.client.get(self.url)
+        self.assertContains(response, text)
 
 
 class HomeFeatureTest(TestCase, BehaviourBase):
