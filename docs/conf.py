2769807d4a5dc46ca3704490f8c8cf3d79f25fbe
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-05-18 00:50:47 +0200

    sphinx doc

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,10 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+os.environ["DJANGO_SETTINGS_MODULE"] = "youarehero.settings.base"
+sys.path.insert(0, os.path.abspath('../src/'))
 
+print sys.path
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
