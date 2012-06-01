35f30cfbaa5a38a34e1311dd06e18911307ec050
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-06-01 02:11:11 +0200

    example split hero home

--- src/herobase/templatetags/hero_actions.py
+++ src/herobase/templatetags/hero_actions.py
@@ -7,4 +7,5 @@ def valid_actions_for(context, instance):
     if not instance:
         return []
     request = context['request']
+    # TODO: make this work with anon user
     return instance.valid_actions_for(request)
