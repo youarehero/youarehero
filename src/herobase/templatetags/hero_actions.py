af2b6b9875ca772ce1cb1360f19757241c3fe267
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-06-01 14:14:07 +0200

    nothing allowed for unathenticated user

--- src/herobase/templatetags/hero_actions.py
+++ src/herobase/templatetags/hero_actions.py
@@ -7,5 +7,6 @@ def valid_actions_for(context, instance):
     if not instance:
         return []
     request = context['request']
-    # TODO: make this work with anon user
+    if not request.user.is_authenticated():
+        return []
     return instance.valid_actions_for(request)
