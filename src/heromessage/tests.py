56f0a459c21597d15b643f79b2624fb7913d459e
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-06-02 16:33:13 +0200

    simple message list test

--- src/heromessage/tests.py
+++ src/heromessage/tests.py
@@ -4,13 +4,18 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from django.core.urlresolvers import reverse
 
 from django.test import TestCase
+from django.test.client import Client
+from herobase.test_factories import create_user
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class MessageIntegrationTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = create_user()
+        self.logged_in = self.client.login(**self.user.credentials)
+    def test_message_list(self):
+        response = self.client.get(reverse('message-list'))
+        self.assertContains(response, 'In')
