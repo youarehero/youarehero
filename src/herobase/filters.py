b306535491a4ffbf6751ac42930732b34e51f015
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-06-05 15:29:42 +0200

    clean up filters

--- src/herobase/filters.py
+++ src/herobase/filters.py
@@ -1,32 +1,18 @@
 import django_filters
+from django.utils.translation import ugettext_lazy as _
 from herobase.models import Quest, CLASS_CHOICES
 
+NO_CHOICE = (('', _('Any')),)
+
 class QuestFilter(django_filters.FilterSet):
-    hero_class = django_filters.ChoiceFilter()
-    level = django_filters.ChoiceFilter(lookup_type='contains')
-    state = django_filters.ChoiceFilter()
+    hero_class = django_filters.ChoiceFilter(choices=NO_CHOICE + CLASS_CHOICES)
+    level = django_filters.ChoiceFilter(lookup_type='contains', choices=NO_CHOICE + Quest.QUEST_STATES)
+    state = django_filters.ChoiceFilter(choices=NO_CHOICE + Quest.QUEST_LEVELS)
+
+    def is_filtered(self):
+        return any(self.data.values())
 
     model = Quest
     fields = ['level', 'state', 'hero_class', 'due_date', 'title', 'description']
-
     class Meta:
         order_by = ['title', 'experience', 'due_date', 'hero_class', 'state']
-
-    CHOICES_FOR_HERO_CLASS = (('', 'All Classes'),) + CLASS_CHOICES
-    CHOICES_FOR_STATE = (('', 'All States'),) + Quest.QUEST_STATES
-    CHOICES_FOR_LEVEL = (('', 'All Levels'),) + Quest.QUEST_LEVELS
-
-    def __init__(self, *args, **kwargs):
-        super(QuestFilter, self).__init__(*args, **kwargs)
-        self.filters['hero_class'].extra.update(
-                {
-                'choices': self.CHOICES_FOR_HERO_CLASS
-            })
-        self.filters['state'].extra.update(
-                {
-                'choices': self.CHOICES_FOR_STATE
-            })
-        self.filters['level'].extra.update(
-                {
-                'choices': self.CHOICES_FOR_LEVEL
-            })
