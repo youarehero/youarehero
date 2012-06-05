e72d8b4a77092d8f9726389912db6ff7b62aed79
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-06-05 19:36:04 +0200

    narrow down filter

--- src/herobase/views.py
+++ src/herobase/views.py
@@ -171,7 +171,9 @@ def leader_board(request):
 
     by_quest_class = {}
     for hero_class, class_name in CLASS_CHOICES:
-        by_quest_class[class_name] = User.objects.filter(adventures__quest__hero_class=hero_class,
+        by_quest_class[class_name] = User.objects.filter(
+            adventures__quest__hero_class=hero_class,
+            adventures__state=Adventure.STATE_OWNER_DONE,
             adventures__quest__state=Quest.STATE_OWNER_DONE)\
             .annotate(class_experience=Sum('adventures__quest__experience'))\
             .order_by('-class_experience')
