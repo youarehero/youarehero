cf7ecc002ff298ff71a709b6186aead3216c7231
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-06-08 17:09:43 +0200

    fix tests

--- src/herobase/__init__.py
+++ src/herobase/__init__.py
@@ -1,4 +1,7 @@
 # hack : change the django auth user defaults
 from django.contrib.auth.models import User
-User._meta.get_field_by_name('email')[0]._unique = True
-User._meta.get_field_by_name('username')[0]._length = 75
+try:
+    User._meta.get_field_by_name('email')[0]._unique = True
+    User._meta.get_field_by_name('username')[0]._length = 75
+except:
+    pass
