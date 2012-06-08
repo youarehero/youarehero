f4ab502c7828318bfe8ca96849d517497d6856cf
Raphael Kimmig <raphael.kimmig@ampad.de>
2012-06-08 17:58:23 +0200

    test email notification tests

--- src/heromessage/tests.py
+++ src/heromessage/tests.py
@@ -9,6 +9,8 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
 from herobase.test_factories import create_user
+from heromessage.models import Message
+from django.core import mail
 
 
 class MessageIntegrationTest(TestCase):
@@ -17,8 +19,23 @@ class MessageIntegrationTest(TestCase):
         self.user = create_user()
         self.logged_in = self.client.login(**self.user.credentials)
     def test_message_list(self):
+        message = Message.objects.create(sender=self.user, recipient=self.user, text='text', title='themailtitle')
         response = self.client.get(reverse('message-list'))
-        self.assertContains(response, 'In')
+        self.assertContains(response, 'themailtitle')
     def test_message_create_view(self):
         response = self.client.get(reverse('message-create'))
         self.assertContains(response, 'text')
+    def test_message_mail_notifiaction_enabled(self):
+        profile = self.user.get_profile()
+        profile.receive_system_email = True
+        profile.receive_private_email = True
+        profile.save()
+        message = Message.objects.create(sender=self.user, recipient=self.user, text='text', title='themailtitle')
+        self.assertIn('themailtitle', mail.outbox[0].subject)
+    def test_message_mail_notifiaction_enabled(self):
+        profile = self.user.get_profile()
+        profile.receive_system_email = False
+        profile.receive_private_email = False
+        profile.save()
+        message = Message.objects.create(sender=self.user, recipient=self.user, text='text', title='themailtitle')
+        self.assertFalse(any(mail.outbox))
