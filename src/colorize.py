4368b7b65fe008fd3c373d677a44c9fc4404608a
Hannes Wächter <hannes.waechter@gmail.com>
2012-10-25 16:16:38 +0200

    colorize images

--- src/colorize.py
+++ src/colorize.py
@@ -1,21 +1,6 @@
+import glob
 from PIL import Image
-
-def replace(image, replace):
-    pixdata = image.load()
-    for y in xrange(image.size[1]):
-        for x in xrange(image.size[0]):
-            for channel, color in replace.items():
-                pixel = pixdata[x, y]
-                zeroes =  set((0,1,2))-set((channel,))
-                if any (pixel[i] for i in zeroes if pixel[i] != 0):
-                    continue
-                intensity = pixel[channel] / 255.0
-                replacement = tuple([int(round(c * intensity)) for c in color] + [pixel[3]])
-                pixdata[x, y] = replacement
-                break
-            else:
-                print pixdata[x, y]
-    return image
+import os
 
 def replace_colors(image, replacemap):
     pixdata = image.load()
@@ -36,7 +21,20 @@ def replace_colors(image, replacemap):
             color = [min(int(c), 255) for c in color]
             pixdata[x, y] = tuple(color + [pixel[3]])
 
-image = Image.open("assets/colorize/Billboard-t.png")
-#image.convert('RGBA')
-replace_colors(image, {0: (33, 144, 4), 1: (200,27,100)})
-image.save("assets/colorize/red.png")
+
+def colorize_images(directory, name, replacemap):
+    if not os.path.exists(os.path.join(directory, name)):
+        os.makedirs(os.path.join(directory, name))
+
+    for file in glob.glob(os.path.join(directory, '*.png')):
+        new_file = os.path.join(directory, name, os.path.basename(file))
+        image = Image.open(file)
+        replace_colors(image, replacemap)
+        image.save(new_file)
+
+#image = Image.open("assets/colorize/Billboard-t.png")
+#replace_colors(image, {0: (33, 144, 4), 1: (200,27,100)})
+#image.save("assets/colorize/red.png")
+
+replacemap = {0: (33, 144, 4), 1: (200,27,100)}
+colorize_images('assets/colorize/', 'test', replacemap)
