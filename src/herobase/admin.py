efde5c637b57adde38eb4bff2050014d06e067d1
Hannes Wächter <hannes.waechter@gmail.com>
2013-01-05 19:21:08 +0100

    new icons and css for them

--- src/herobase/admin.py
+++ src/herobase/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from herobase.models import UserProfile, Quest, Adventure
 from heromessage.models import Message
-
+from herorecommend.models import UserCombinedProfile,UserRatingProfile, UserSelectionProfile, SkillBase, QuestProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
     pass
@@ -10,3 +10,5 @@ admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Quest)
 admin.site.register(Adventure)
 admin.site.register(Message)
+admin.site.register(UserCombinedProfile)
+admin.site.register(QuestProfile)
