318fb44d60b18967133f6588082cdda9a137a112
Hannes Wächter <hannes.waechter@gmail.com>
2013-02-07 17:38:38 +0100

    .box collapsible

--- src/herobase/static/js/herobase.js
+++ src/herobase/static/js/herobase.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-    $(document).on('click', '.box .collapsible', function(e) {
-        var headline = $(this);
-        var box = headline.closest('.box');
-        var content = box.find()
-    })
+    $(document).on('click', '.box.collapsible .box-title', function(e) {
+        var $headline = $(this);
+        var $box = $headline.closest('.box');
+        var $content = $box.find('.box-content');
+        if ($box.hasClass('collapsed')) {
+            $box.removeClass('collapsed');
+            $content.slideDown();
+        } else {
+            $box.addClass('collapsed');
+            $content.slideUp();
+        }
+    });
 });
