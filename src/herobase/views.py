bc44dc934c9dfad034a67378a69b567f4dad685d
Raphael Kimmig <raphael.kimmig@ampad.de>
2013-02-26 18:59:39 +0100

    reduce leaderboard queries

--- src/herobase/views.py
+++ src/herobase/views.py
@@ -357,7 +357,7 @@ def userprofile_skill_settings(request):
 @login_required
 def leader_board(request):
     """Render a view of the top heroes by rank."""
-    global_board = User.objects.select_related().filter(profile__experience__gt=0).order_by('-profile__experience')[:10]
+    global_board = User.objects.select_related('profile').filter(profile__experience__gt=0).order_by('-profile__experience')[:10]
     relativ_board = []
 
     return render(request, "herobase/leader_board.html", {
