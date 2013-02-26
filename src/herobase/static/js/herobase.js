8b07dfdc4ad8e230998c9580e9676934c3684946
Raphael Kimmig <raphael.kimmig@ampad.de>
2013-02-26 18:09:28 +0100

    show created date

--- src/herobase/static/js/herobase.js
+++ src/herobase/static/js/herobase.js
@@ -13,4 +13,18 @@ $(document).ready(function() {
     });
 
     $('[data-toggle=tooltip]').tooltip();
+
+    $("#hero-cancel").submit(function (e) {
+         var confirmed = confirm("Wollen sie wirklich ihre Teilnahme an der Quest zurückziehen?");
+        if(!confirmed) {
+            e.preventDefault();
+        }
+    });
+
+    $("#owner-cancel").submit(function (e) {
+        var confirmed = confirm("Wollen sie diese Quest wirklich löschen?");
+        if(!confirmed) {
+            e.preventDefault();
+        }
+    });
 });
