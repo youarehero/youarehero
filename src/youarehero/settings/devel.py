baf6cb814365a5209f75407b33c7806ab44c8fc5
Hannes Wächter <hannes.waechter@gmail.com>
2013-06-22 12:11:08 +0200

    mail panel settings in devel settings

--- src/youarehero/settings/devel.py
+++ src/youarehero/settings/devel.py
@@ -15,3 +15,37 @@ DATABASES = {
     }
 }
 
+DEBUG_TOOLBAR_ENABLED = False
+
+if DEBUG_TOOLBAR_ENABLED:
+    INSTALLED_APPS += ('debug_toolbar',
+                       'mail_debug_panel',
+    )
+
+    INTERNAL_IPS = ('127.0.0.1', )
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        }
+
+    COVERAGE_MODULE_EXCLUDES += ('debug_toolbar', )
+
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+        'mail_debug_panel.panels.MailDebugPanel',
+    )
+
+    EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'noreply@localhost'
