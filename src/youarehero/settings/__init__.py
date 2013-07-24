a0a90e973ec73eb9092f61c6e711e146c8b05622
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2013-07-24 13:55:26 +0200

    Try to load local settings before falling back to devel
    This keeps current setups working, while still allowing youarehero to be run
    without any configuration.
    
    We don't just catch ImportError because that could mask some valid issues.

--- src/youarehero/settings/__init__.py
+++ src/youarehero/settings/__init__.py
@@ -1,2 +1,8 @@
 #noinspection PyUnresolvedReferences
-from devel import *
+from os import path
+
+settings_dir = path.dirname(__file__)
+if path.exists(path.join(settings_dir, 'local.py')):
+    from .local import *
+else:
+    from .devel import *
