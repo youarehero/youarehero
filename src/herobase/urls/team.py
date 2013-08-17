ca488377f566c8e49b74a9f8246297a8f439488d
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2013-08-17 15:45:35 +0200

    Added links from the profile to the team page and back

--- src/herobase/urls/team.py
+++ src/herobase/urls/team.py
@@ -2,10 +2,11 @@
 import logging
 
 from django.conf.urls import patterns, url
+from herobase.views import team
 
 logger = logging.getLogger(__name__)
 
 urlpatterns = patterns(
     'herobase.views',
-    url(regex=r'(?P<team_name>.+)$', view="team", name="team")
+    url(regex=r'(?P<team_name>.+)$', view=team, name="team")
 )
