3869b89321eef439c9acc5aa8ee4c4b4084087b9
Raphael Kimmig <raphael.kimmig@ampad.de>
2013-10-04 12:54:53 +0200

    use creation instead of modification date for edit window

--- src/herobase/models.py
+++ src/herobase/models.py
@@ -353,11 +353,11 @@ class Quest(LocationMixin, models.Model):
         """Get the url for this quests detail page."""
         return reverse("quest_detail", args=(self.pk,))
 
-    EDIT_WINDOW_MINUTES = 10
+    EDIT_WINDOW_MINUTES = 30
 
     @property
     def edit_window_expired(self):
-        return (now() - self.modified) > timedelta(minutes=self.EDIT_WINDOW_MINUTES)
+        return (now() - self.created) > timedelta(minutes=self.EDIT_WINDOW_MINUTES)
 
     def get_state_display(self):
         if self.canceled:
