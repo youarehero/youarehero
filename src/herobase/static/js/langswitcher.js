1d4fe4f4c249fd928f85fdf27b49534e1eccba71
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2013-12-12 17:51:03 +0100

    use data- instead of x- for custom html attributes to conform to standards

--- src/herobase/static/js/langswitcher.js
+++ src/herobase/static/js/langswitcher.js
@@ -1,5 +1,5 @@
 $(document).on("click", ".langswitcher", function(event) {
-    $.cookie('django_language', event.currentTarget.attributes['x-target-lang'].value);
+    $.cookie('django_language', event.currentTarget.attributes['data-target-lang'].value);
     location.reload(true);
     return false;
 });
